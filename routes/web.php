0a9e720eb9f761eeb7ab6c484e03ea718fc59f31
Oskar Lohse <oskar@baucampus.de>
2023-03-03 16:25:18 +0100

    5

--- routes/web.php
+++ routes/web.php
@@ -65,7 +65,8 @@ $domains = [
 
 foreach ($domains as $domain => $domainData) {
 Route::domain($domain)->group(function () use ($routes, $domainData) {
-    Route::get('/', [OrteatController::class, 'index']);
+    Route::get('/', [OrteatController::class, 'index'])
+    ->with('domainort', $domainData['domainort']);
     Route::get('/gutachter/{gutachter}', [GutachterController::class, 'show'], function (Request $request){});
     Route::get('/{ort}/bausachverstaendiger', [OrteatController::class, 'show'], function () use ($domainData) {})
             ->middleware('cache.headers:private;max_age=3600');
