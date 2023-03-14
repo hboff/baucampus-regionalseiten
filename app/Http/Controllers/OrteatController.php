182bf67540c92d56e524807bb07615cc2bfb59e6
Oskar Lohse <oskar@baucampus.de>
2023-03-14 14:06:14 +0100

    test 9

--- app/Http/Controllers/OrteatController.php
+++ app/Http/Controllers/OrteatController.php
@@ -238,7 +238,7 @@ class OrteatController extends Controller
                  })
                  ->get();
         
-        $cityData = DB::table('city_data')->select('laenge', 'breite')->where('stadt', $ortat)->first();
+        $cityData = DB::table('city_data')->select('laenge', 'breite')->where('stadt', $ort)->first();
         $laengengrad = $cityData->laenge;
         $breitengrad = $cityData->breite;
 
