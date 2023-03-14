69b0c078b07348b4eb8190801fb37721ce2969dd
Oskar Lohse <oskar@baucampus.de>
2023-03-14 13:47:53 +0100

    test 6

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -13,7 +13,7 @@ $p = 0;
 @foreach ($expert as $row)
 
 @if ($p++ < 2)
-@if ($ortsname == $row->ort)
+@if ($ortsname == $row->stadt)
   <div class="profile">
     <img src="/img/{{$row->Photo}}" alt="" class="mt-4" style="    vertical-align: middle;
 width: 180px;
