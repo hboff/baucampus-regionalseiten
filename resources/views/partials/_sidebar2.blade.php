69876004743e8cfba266405a0520429e5279d481
Oskar Lohse <oskar@baucampus.de>
2023-03-15 18:07:41 +0100

    test 11

--- resources/views/partials/_sidebar2.blade.php
+++ resources/views/partials/_sidebar2.blade.php
@@ -11,20 +11,7 @@ $p = 0;
 @isset($expert)
 
 @foreach ($expert as $row)
-@if ($p++ < 2)
-
-  <div class="profile">
-    <img src="/img/{{$row->Photo}}" alt="" class="mt-4" style="    vertical-align: middle;
-width: 180px;
-height: 180px;
-border-radius: 50%;
-object-fit: cover;
-object-position: center;">
-
-    <h1 class="text-light mb-3"><a href="/gutachter/{{$row->LastName}}">{{$row->FirstName}} {{$row->LastName}}</a></h1>
-    <p class="text-light text-center mb-0">{{$row->Phone}}</p>
-    <p class="text-light text-center">{{$row->Email}}</p>
-  </div>
+{{$row->stadt}}
 @else
 <div class='mt-1'></div>
 @endif
