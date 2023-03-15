08fbe6cea2d9dda2e707e47812bea0bcdf6af0fa
Oskar Lohse <oskar@baucampus.de>
2023-03-15 14:43:20 +0100

    test 4

--- resources/views/partials/_footer1.blade.php
+++ resources/views/partials/_footer1.blade.php
@@ -27,6 +27,7 @@ object-position: center;" class="description"><br>
 @if($cities->stadt == $ort->stadt)
 <a href="/{{$ort->stadt_umlaut}}/bausachverstaendiger" class="link-dark">Baugutachter {{$ort->stadt}}</a>
 @endif
+@endif
 @endforeach
 @endisset
 @endisset
