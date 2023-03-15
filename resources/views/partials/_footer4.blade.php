acfb13e05ae16b3826ca083271f6782f3c45fd4d
Oskar Lohse <oskar@baucampus.de>
2023-03-15 14:06:43 +0100

    footer unterseiten

--- resources/views/partials/_footer4.blade.php
+++ resources/views/partials/_footer4.blade.php
@@ -19,7 +19,7 @@ object-position: center;" class="description"><br>
                 $i=0;
                 @endphp        
    
-    @foreach($results as $cities)
+    @foreach($data as $cities)
     @if ($i++ < 16)
     <a class="link-dark" href="/{{$cities->stadt_umlaut}}/bausachverstaendiger">Baugutachter {{$cities->stadt}}</a>
     @else
