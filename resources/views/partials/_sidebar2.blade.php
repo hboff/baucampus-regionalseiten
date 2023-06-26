1eb4be330e66b63a0358a90697634708ddb0858f
Oskar Lohse <oskar@baucampus.de>
2023-06-26 11:17:28 +0200

    test 14

--- resources/views/partials/_sidebar2.blade.php
+++ resources/views/partials/_sidebar2.blade.php
@@ -50,9 +50,8 @@ object-position: center;">
 
 
 
+<!-- ======= Offcanvas Sidebar ======= -->
 
-
-<!-- ======= Header ======= -->
 <div class="offcanvas offcanvas-start d-flex flex-column" style="background: #4d636f;" id="sidebarprofile-offcanvas">
 
   <!-- ======= profile1 ======= -->
@@ -67,12 +66,7 @@ $p = 0;
 @if ($p++ < 2)
 
   <div class="profile">
-    <img src="/img/{{$row->Photo}}" alt="" class="mt-4" style="    vertical-align: middle;
-width: 180px;
-height: 180px;
-border-radius: 50%;
-object-fit: cover;
-object-position: center;">
+    <img src="/img/{{$row->Photo}}" alt="" class="mt-4 profile-image">
 
     <h1 class="text-light mb-3"><a href="/gutachter/{{$row->LastName}}">{{$row->FirstName}} {{$row->LastName}}</a></h1>
     <p class="text-light text-center mb-0">{{$row->Phone}}</p>
@@ -87,15 +81,9 @@ object-position: center;">
 @endisset
 
   <div class="profile mt-4">
-    <img src="/img/logo.png" alt="" class="mt-2" style="vertical-align: middle;
-width: 120px;
-height: 120px;
-border-style:none;
-object-fit: cover;
-object-position: center;">
+    <img src="/img/logo.png" alt="" class="mt-2 profile-image">
     <h1 class="text-light mb-3"><a href="index.html">Hauptstelle Baucampus</a></h1>
     <p class="text-light text-center mb-0">Telefon: 05722 913800</p>
     <p class="text-light text-center">kontakt@baucampus.de</p>
   </div>
 </div>
-</header><!-- End Header -->
